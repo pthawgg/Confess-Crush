715643439fadb1d4b73ee8da37c783a1fc6268b9
pthawgg <101125175+pthawgg@users.noreply.github.com>
2022-03-07 22:47:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    introTitle: 'Gửi Phương Chi đáng yêu của anh',
+    introDesc: `Bởi vì em quá xinh đẹpp
+    quá đỗi yêu kiều
+    Xung quanh bao nhiu cậu trai mún chiếm tim emm
+    Nhưng chỉ có a ở đây đấm chết chúng nó!!!`,
+    btnIntro: '^^Bắt đầuu^^',
+    title: 'Chúc bé ngày 8/3 đầu tiên với ngiu thật hạnh phúcc 💘',
+    desc: 'Yêu em nhiều lắmm, z còn ghét anh kk??? ',
+    btnYes: 'Hih yeu anh nhieu',
+    btnNo: 'Coáaa',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bé lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    mess: 'Anh biết mà 🥰. Yêu bé nhiềuuuuu',
+    messDesc: 'Bên anh lâu nhất có thể nhée☺️',
+    btnAccept: 'Dạaa <3',
+    messLink: 'https://p.thawgg/yeu.pchii
 }
